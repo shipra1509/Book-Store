4330f11cdc6be6b3c0c8d103baa8e69fd43a8502
Shipra Sinha <shipra.juet@gmail.com>
2016-08-31 01:29:03 +0000

    Basic changes in layout

--- js/app.js
+++ js/app.js
@@ -26,10 +26,18 @@ $('#slider').rotateSlider({
 });
 
 $('#myTabs a').click(function (e) {
-  e.preventDefault()
-  $(this).tab('show')
+	e.preventDefault()
+	$(this).tab('show')
 })
 
+
+$('.search-panel .dropdown-menu').find('a').click(function(e) {
+	e.preventDefault();
+	var param = $(this).attr("href").replace("#","");
+	var concept = $(this).text();
+	$('.search-panel span#search_concept').text(concept);
+	$('.input-group #search_param').val(param);
+});
 
 
 });
