850bbbb0c2bb01a78e2efe7c3dc388b9872848d0
Shipra Sinha <shipra.juet@gmail.com>
2016-09-02 11:36:40 +0000

    Removed the bug of overflowing

--- js/app.js
+++ js/app.js
@@ -1,43 +1,38 @@
 $(document).ready(function()    {
 
-  // Instantiate the Bootstrap carousel
-  $('.multi-item-carousel').carousel({
-  	interval: false
-  });
+	$('.multi-item-carousel').carousel({
+		interval: false
+	});
 
-// for every slide in carousel, copy the next slide's item in the slide.
-// Do the same for the next, next item.
-$('.multi-item-carousel .item').each(function(){
-	var next = $(this).next();
-	if (!next.length) {
-		next = $(this).siblings(':first');
-	}
-	next.children(':first-child').clone().appendTo($(this));
+	$('.multi-item-carousel .item').each(function(){
+		var next = $(this).next();
+		if (!next.length) {
+			next = $(this).siblings(':first');
+		}
+		next.children(':first-child').clone().appendTo($(this));
 
-	if (next.next().length>0) {
-		next.next().children(':first-child').clone().appendTo($(this));
-	} else {
-		$(this).siblings(':first').children(':first-child').clone().appendTo($(this));
-	}
-});
+		if (next.next().length>0) {
+			next.next().children(':first-child').clone().appendTo($(this));
+		} else {
+			$(this).siblings(':first').children(':first-child').clone().appendTo($(this));
+		}
+	});
 
-$('#slider').rotateSlider({
+	$('#slider').rotateSlider({
 
-});
+	});
 
-$('#myTabs a').click(function (e) {
-	e.preventDefault()
-	$(this).tab('show')
-})
+	$('#myTabs a').click(function (e) {
+		e.preventDefault()
+		$(this).tab('show')
+	})
 
-
-$('.search-panel .dropdown-menu').find('a').click(function(e) {
-	e.preventDefault();
-	var param = $(this).attr("href").replace("#","");
-	var concept = $(this).text();
-	$('.search-panel span#search_concept').text(concept);
-	$('.input-group #search_param').val(param);
-});
-
+	$('.search-panel .dropdown-menu').find('a').click(function(e) {
+		e.preventDefault();
+		var param = $(this).attr("href").replace("#","");
+		var concept = $(this).text();
+		$('.search-panel span#search_concept').text(concept);
+		$('.input-group #search_param').val(param);
+	});
 
 });
